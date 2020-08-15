4f706c5c859965fb2389aff3caa845669798c2e8
s2vArya <shivams2varya@gmail.com>
2020-08-16 00:40:01 +0530

    Initialising Weather App

---
+++ app/src/main/java/com/example/weatherforecast/SettingsActivity.java
@@ -0,0 +1,24 @@
+package com.example.weatherforecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        SetToolbar();
+    }
+
+    private void SetToolbar() {
+        Toolbar toolbar = findViewById(R.id.appbar);
+        toolbar.setTitle("Settings");
+
+        setSupportActionBar(toolbar);
+    }
+}
